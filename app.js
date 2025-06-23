14862d4f077c166194b3724a94352deee70754c8
ErikHedaker <erikhedaker@gmail.com>
2025-06-23 15:53:17 +0200

    Removed leftover file

--- app.js
+++
@@ -1,9 +0,0 @@
-function onOpen(event) {
-    const spreadsheet = event.source;
-    const sheet = {
-        input: spreadsheet.getSheetByName(`Ärende`),
-        output: spreadsheet.getSheetByName(`Statistisk`),
-    };
-    const manager = new StatisticsManager(Errand.from(sheet.input));
-    console.log(String(manager));
-}
